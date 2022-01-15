4a83f9c08ff7eb7121e698033d6b847cf6adfe5a
2theMoon <dlaguddh1@gmail.com>
2022-01-15 20:16:10 +0900

    타이핑 효과 적용
    타입 임의 지정

--- src/Hello.tsx
+++ src/Hello.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import bgimg from "./img/helloBg.gif";
+import { Typing } from "typing-effect-reactjs";
 
 const Section = styled.div`
     width: 100%;
@@ -9,21 +10,43 @@ const Section = styled.div`
     background-repeat:no-repeat;
     background-size: cover;
     position: relative;
-`;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    `;
 
 const Title = styled.div`
     font-size: 40px;
-    top: 50%;
-    left: 45%;
     position: absolute;
-`;
+    `;
 
 
 function Hello() {
-
+    const textArry = ['퍼블리싱', 'UI & UX', '리소스 관리', 'API 데이터 다루기', '디자이너 백엔드 소통', '다 할 수 있는!'];
+    const myName = ['................', '임형오 입니다!'];
     return (
         <Section>
-            <Title>안녕하세요</Title>
+            <Title>
+                프론트엔드 개발자란?
+
+                <Typing
+                    text={textArry}
+                    typeSpeed={300}
+                    deleteSpeed={200}
+                    disablebBlinkingOnEnd={false}
+                    blinkingSpeed={530}
+                    letterSpacing={0}
+                    cursorPadding={0.15}
+                    cursorColor={"red"}
+                />
+                <Typing
+                    text={myName}
+                    ignoreInitialDelay={true}
+                    typeSpeed={800}
+                    deleteSpeed={200}
+                    disablebBlinkingOnEnd={true}
+                />
+            </Title>
         </Section >
     );
 }
